242c04f53e1ee75347a163bbad74c47b9ecd245c
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2025-07-19 15:38:50 +0500

    Fri 19 15:38; Finally TLS worked

--- pod/settings/my_taskiq.py
+++ pod/settings/my_taskiq.py
@@ -1,5 +1,3 @@
-import ssl
-
 from taskiq import TaskiqScheduler
 from taskiq.schedule_sources import LabelScheduleSource
 from taskiq_redis import ListQueueBroker, RedisAsyncResultBackend, RedisScheduleSource
@@ -8,34 +6,19 @@ from settings.my_config import get_settings
 
 settings = get_settings()
 
-ssl_params = {
-    "ssl_ca_certs": str(settings.CA_PATH),
-    "ssl_certfile": str(settings.FASTAPI_CLIENT_CERT_PATH),
-    "ssl_keyfile": str(settings.FASTAPI_CLIENT_KEY_PATH),
-    "ssl_cert_reqs": ssl.CERT_REQUIRED,
-    "ssl_check_hostname": True,
-}
-
-redis_url = f"rediss://@{settings.REDIS_HOST}:6379"
-
-ssl_context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=str(settings.CA_PATH))
-ssl_context.load_cert_chain(certfile=str(settings.FASTAPI_CLIENT_CERT_PATH), keyfile=str(settings.FASTAPI_CLIENT_KEY_PATH))
-ssl_context.check_hostname = True
-ssl_context.verify_mode = ssl.CERT_REQUIRED
+redis_url = f"rediss://@{settings.REDIS_HOST}:6379?ssl_cert_reqs=required&ssl_ca_certs={str(settings.CA_PATH)}&ssl_certfile={str(settings.FASTAPI_CLIENT_CERT_PATH)}&ssl_keyfile={str(settings.FASTAPI_CLIENT_KEY_PATH)}"
 
 broker = ListQueueBroker(
     url=redis_url,
-    ssl=ssl_context,
-).with_result_backend(result_backend=RedisAsyncResultBackend(
-    redis_url=redis_url,
-    result_ex_time=600,
-    ssl=ssl_context,
-),
+).with_result_backend(
+    result_backend=RedisAsyncResultBackend(
+        redis_url=redis_url,
+        result_ex_time=600,
+    )
 )
 
 redis_schedule_source = RedisScheduleSource(
     url=redis_url,
-    ssl=ssl_context,
 )
 
 scheduler = TaskiqScheduler(
