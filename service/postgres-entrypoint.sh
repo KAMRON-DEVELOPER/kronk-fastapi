85fcad810200a14c02095c9a71fae84b33369530
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2025-07-19 10:24:55 +0500

    Fri 19 10:24; tls one more try with compose

--- service/postgres-entrypoint.sh
+++ service/postgres-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+set -e
+
 # Copy private key from secrets
-cp /run/secrets/pg_server_key.pem /tmp/pg_server_key.pem
+cp /run/secrets/pg-server-key.pem /tmp/pg-server-key.pem
+chown postgres:postgres /tmp/pg-server-key.pem
+chmod 600 /tmp/pg-server-key.pem
 
-# Set correct permissions for postgres user
-chown postgres:postgres /tmp/pg_server_key.pem
-chmod 600 /tmp/pg_server_key.pem
-
-# Run PostgreSQL with the correct config as postgres user
-exec gosu postgres postgres -c config_file=/etc/postgresql/postgresql.conf -c ssl_key_file='/tmp/pg_server_key.pem'
+# Pass control to the official entrypoint
+exec docker-entrypoint.sh postgres -c config_file=/etc/postgresql/postgresql.conf -c ssl_key_file='/tmp/pg-server-key.pem'
