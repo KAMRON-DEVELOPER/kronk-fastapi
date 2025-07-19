8003bd31e648a8b6d90b41b9d0567b0062618826
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2025-07-19 18:42:01 +0500

    Fri 19 15:38; Finally TLS workedclear

--- pod/settings/my_taskiq.py
+++ pod/settings/my_taskiq.py
@@ -8,20 +8,8 @@ settings = get_settings()
 
 redis_url = f"rediss://@{settings.REDIS_HOST}:6379?ssl_cert_reqs=required&ssl_ca_certs={str(settings.CA_PATH)}&ssl_certfile={str(settings.FASTAPI_CLIENT_CERT_PATH)}&ssl_keyfile={str(settings.FASTAPI_CLIENT_KEY_PATH)}"
 
-broker = ListQueueBroker(
-    url=redis_url,
-).with_result_backend(
-    result_backend=RedisAsyncResultBackend(
-        redis_url=redis_url,
-        result_ex_time=600,
-    )
-)
+broker = ListQueueBroker(url=redis_url).with_result_backend(result_backend=RedisAsyncResultBackend(redis_url=redis_url, result_ex_time=600))
 
-redis_schedule_source = RedisScheduleSource(
-    url=redis_url,
-)
+redis_schedule_source = RedisScheduleSource(url=redis_url)
 
-scheduler = TaskiqScheduler(
-    broker=broker,
-    sources=[LabelScheduleSource(broker=broker), redis_schedule_source],
-)
+scheduler = TaskiqScheduler(broker=broker, sources=[LabelScheduleSource(broker=broker), redis_schedule_source])
